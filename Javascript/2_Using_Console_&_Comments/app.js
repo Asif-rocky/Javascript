4d2e58af0ad63d4af9c7fc9d53514060145ee7ef
Asif-rocky <asif13199615@gmail.com>
2026-01-07 07:07:09 +0530

    USing console and comments

---
+++ Javascript/2_Using_Console_&_Comments/app.js
@@ -0,0 +1,11 @@
+// Using console
+console.log("Hello Asif!!");
+console.warn("THis is a warning !!!!");
+console.table({ name: "Asif", age: 29 });
+
+console.clear();
+console.log("Console is cleared");
+
+// Comments
+// Above code written shows how to use console and its methods
+// Shortcut for console.log('') is clg or log
