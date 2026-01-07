715416142fca9d13100544f54552d0a4dca345f2
Asif-rocky <asif13199615@gmail.com>
2026-01-07 07:42:30 +0530

    Numbers

---
+++ Javascript/4_Numbers/app.js
@@ -0,0 +1,55 @@
+let num = 10;
+num = 10.05;
+num = -10;
+
+console.log(num);
+console.log(typeof num);
+
+//Basic Math
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+console.clear();
+
+// Incrementing
+let counter = 0;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+console.log(counter);
+
+// Decrementing
+let timer = 60;
+timer--;
+timer--;
+timer--;
+timer--;
+timer--;
+console.log(timer);
+
+console.clear();
+
+// Challenge
+// 1. Create variable name (firstFavNum) & store your number;
+// 2. Create variable name (secondFavNum) & store your number;
+// 3. Add firstFavNum & secondFavNUmber
+// 4. Subtract firstFavNum & secondFavNUmber
+// 5. Multiply firstFavNum & secondFavNUmber
+// 6. Divide firstFavNum & secondFavNUmber
+// 7. Modulus of firstFavNum & secondFavNUmber
+// 8. POwer of firstFavNum & secondFavNUmber
+
+let firstFavNum = 13;
+let secondFavNum = 3;
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
