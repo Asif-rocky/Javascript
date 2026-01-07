252f685bff132c047c6010f45d3c4bc401f02530
Asif-rocky <asif13199615@gmail.com>
2026-01-07 07:52:13 +0530

    Booleans

---
+++ Javascript/5_Booleans/app.js
@@ -0,0 +1,26 @@
+// True or False
+let isTrue = false;
+console.log(isTrue);
+
+let isFalse = false;
+console.log(isFalse);
+
+// Nan - Not a Number
+let nan = NaN;
+console.log(nan);
+
+let number = 10 + undefined;
+console.log(number); // NaN
+
+// falsy values
+// false, null, undefined, 0 , -0, NaN, '', "", ``
+
+// truthy values
+// true, number, string, array etc
+
+// null and undefined (null set by programmer, undefined set by compiler of javascript)
+let notDefined = undefined;
+console.log(notDefined); // undefined
+
+let myValue = null;
+console.log(myValue);
