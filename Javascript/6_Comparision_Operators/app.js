290d485fb9970e67de2f11c55478e9df07f22a0c
Asif-rocky <asif13199615@gmail.com>
2026-01-09 17:07:14 +0530

    Comparision Operators

---
+++ Javascript/6_Comparision_Operators/app.js
@@ -0,0 +1,34 @@
+//Comparision Operators - Relational & Equality
+
+//Relational Operators
+// > Greater than
+console.log(10 > 10); //false
+
+// < Less than
+console.log(10 < 10); //false
+console.log(10 < 50); //true
+
+// >= Greater than or equal to
+console.log(10 >= 10); //true
+
+// <= Less than or equal to
+console.log(10 <= 10); //true
+
+console.clear();
+
+//Equality Operators
+// === strict equality (type + value)
+console.log(10 === 10); //true
+console.log(10 === "10"); //false
+
+// !== strict non-equality (type + value)
+console.log(10 !== 10); //false
+console.log(10 !== "10"); //true
+
+// == lose equality operator (value)
+console.log(10 == 10); //true
+console.log(10 == "10"); //true
+
+// != not equality operator (value)
+console.log(10 != 10); //false
+console.log(10 != "10"); //false
