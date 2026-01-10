70ff11a96423ec136a7fd9c75e698ae23dc97a59
Asif-rocky <asif13199615@gmail.com>
2026-01-10 07:07:21 +0530

    Type Conversions

---
+++ Javascript/8_Type_Conversions/app.js
@@ -0,0 +1,24 @@
+let money = "50";
+
+// Convert string to number
+// money = parseInt(money);
+// money = +money;
+money = Number(money);
+console.log(typeof money);
+
+console.clear();
+
+// Convert number to string
+let secondMoney = 30;
+// secondMoney = secondMoney.toString();
+secondMoney = String(secondMoney);
+console.log(secondMoney);
+console.log(typeof secondMoney);
+
+console.clear();
+
+//Convert string to decimal
+let thirdMoney = "50.245";
+thirdMoney = parseFloat(thirdMoney);
+console.log(thirdMoney);
+console.log(typeof thirdMoney);
