6d382e057abdaa46c0a03bf4c22fdf9b75f66623
Asif-rocky <asif13199615@gmail.com>
2026-01-10 07:40:55 +0530

    For Loop

---
+++ Javascript/11_For_Loop/app.js
@@ -0,0 +1,12 @@
+// Log Apple 10 times
+for (let i = 0; i < 10; i++) {
+  console.log("Apple");
+}
+
+//Nested loop
+for (let i = 0; i < 10; i++) {
+  console.log(`Inner loop: ${i}`);
+  for (let j = 0; j < 5; j++) {
+    console.log(`Outer loop: ${j}`);
+  }
+}
