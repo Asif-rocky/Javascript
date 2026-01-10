c34bba00d9602ad7be579214113e29b870021c25
Asif-rocky <asif13199615@gmail.com>
2026-01-10 07:58:09 +0530

    Do while loop

---
+++ Javascript/13_Do_While_Loop/app.js
@@ -0,0 +1,18 @@
+// do
+//   BODY
+// while (condition);
+
+let i = 1;
+
+do {
+  console.log("From Do");
+  i++;
+} while (i <= 5);
+
+//Wrong code
+// do {
+//   console.log("From Do");
+// } while (i <= 5)
+// {
+//       i++;
+// }
