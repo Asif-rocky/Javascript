429dfd5efc192e93b2efcb0f54e5840d8d9594f4
Asif-rocky <asif13199615@gmail.com>
2026-01-10 07:22:25 +0530

    Conditional Statements

---
+++ Javascript/9_Conditional_Statements/app.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 10;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("a is less than b");
+} else {
+  console.log("Both a and b are equal");
+}
