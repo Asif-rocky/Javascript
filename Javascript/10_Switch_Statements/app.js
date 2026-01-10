8098592fa92eaaa90b2d0211292a93eff142e8f8
Asif-rocky <asif13199615@gmail.com>
2026-01-10 07:33:51 +0530

    Switch Statements

---
+++ Javascript/10_Switch_Statements/app.js
@@ -0,0 +1,13 @@
+let x = 0;
+
+switch (x) {
+  case 0:
+    console.log("Value is 0");
+    break;
+  case 1:
+    console.log("Value is 1");
+    break;
+  default:
+    console.log("No value is found");
+    break;
+}
