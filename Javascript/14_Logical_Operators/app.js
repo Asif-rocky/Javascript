40470eeb61c433436a3b7cd9aff60be88a593ae2
Asif-rocky <asif13199615@gmail.com>
2026-01-13 08:03:03 +0530

    Logical Operators

---
+++ Javascript/14_Logical_Operators/app.js
@@ -0,0 +1,15 @@
+// 1. Loigcal AND (&&)
+const a = true;
+const b = false;
+const c = 4;
+
+console.log(a && b); // false
+console.log(c > 2 && c < 2); // false
+
+// 2. Logical OR (||)
+console.log(a || b); // true
+console.log(c > 2 || c < 2); // true
+
+// 3. Logical NOT(!)
+console.log(!a); // false
+console.log(!b); // true
