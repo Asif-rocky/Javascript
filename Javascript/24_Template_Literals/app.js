035efca838f25ea6634eb81cb777d0d089331581
Asif-rocky <asif13199615@gmail.com>
2026-01-20 07:35:42 +0530

    Template Literals

---
+++ Javascript/24_Template_Literals/app.js
@@ -0,0 +1,10 @@
+console.log(`This code has
+new line`);
+
+const firstName = "Asif";
+const lastName = "Ali";
+console.log(`My name is ${firstName} ${lastName}`);
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
