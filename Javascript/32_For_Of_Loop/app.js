5986e3bb99bfb372829d2615a17c0585374b4c1c
Asif-rocky <asif13199615@gmail.com>
2026-01-25 07:16:24 +0530

    For Of Loop

---
+++ Javascript/32_For_Of_Loop/app.js
@@ -0,0 +1,10 @@
+let peoples = ["Alice", "Bob", "Charlie"];
+
+for (let person of peoples) {
+  console.log(person);
+}
+
+const programmingLang = "JavaScript";
+for (let char of programmingLang) {
+  console.log(char);
+}
