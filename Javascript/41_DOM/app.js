674cb0be4e67dceb739e81865da99907105a549d
Asif-rocky <asif13199615@gmail.com>
2026-02-05 07:08:29 +0530

    DOM Introduction

---
+++ Javascript/41_DOM/app.js
@@ -0,0 +1,7 @@
+// DOM means Document Object Model
+// It represents the structure of a web document as a tree of objects
+
+console.dir(document); // Displays the document object in the console
+console.log(document.baseURI); // Logs the base URI of the document
+console.log(document.location); // Logs the location object of the document
+console.log(location.host); // Logs the host of the current URL
