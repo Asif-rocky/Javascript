88396b923900642b2003bae1e5faba49d12c053e
Asif-rocky <asif13199615@gmail.com>
2026-02-17 18:11:54 +0530

    Modifying styles

---
+++ Javascript/47_Modifying_Styles/app.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector("h1");
+console.log(h1.style); //inline styles
+
+h1.style.color = "teal"; //setting inline style
+h1.style.backgroundColor = "orange";
+
+/* Note: Avoid setting inline styles like above instead use classes */
